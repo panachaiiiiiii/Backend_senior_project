9873295845e870a93f2fa5fa425b8f18332887e1
panachaiiiiiii <gasza60@gmail.com>
2026-03-24 21:20:42 +0700

    path

--- core/firebase_config.py
+++ core/firebase_config.py
@@ -1,7 +1,16 @@
 import firebase_admin
 from firebase_admin import credentials, db
+import os
+import json
 
-cred = credentials.Certificate("serviceAccountKey.json")
+firebase_key = os.getenv("FIREBASE_KEY")
+
+if not firebase_key:
+    raise ValueError("FIREBASE_KEY is missing")
+
+cred_dict = json.loads(firebase_key)
+
+cred = credentials.Certificate(cred_dict)
 
 firebase_admin.initialize_app(cred, {
     "databaseURL": "https://senior-b3690-default-rtdb.asia-southeast1.firebasedatabase.app"
