944eae6b651b0d973133055f0c3609150b487c8a
panachaiiiiiii <gasza60@gmail.com>
2026-03-24 22:34:33 +0700

    deploy

--- database/firebase.py
+++ database/firebase.py
@@ -1,19 +1,17 @@
 import firebase_admin
 from firebase_admin import credentials, db
-import os
-import json
+import os, json
 
 def get_db():
     if not firebase_admin._apps:
         firebase_key = os.getenv("FIREBASE_KEY")
 
         if not firebase_key:
-            raise ValueError("FIREBASE_KEY not found in environment")
+            raise Exception("FIREBASE_KEY not found")
 
         cred_dict = json.loads(firebase_key)
 
         cred = credentials.Certificate(cred_dict)
-
         firebase_admin.initialize_app(cred, {
             "databaseURL": os.getenv("DATABASE_URL")
         })
